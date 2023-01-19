44d93fc4a41606bb85231a99c016fa3bd509324c
Riya379 <122305024+Riya379@users.noreply.github.com>
2023-01-19 21:37:33 +0530

    submitting q1 of a1

---
+++ A1_q1.cpp
@@ -0,0 +1,15 @@
+1.#include<iostream>
+using namespace std;
+int main(int argc, char** agrc) {
+  //write your code here
+#include <iostream>
+
+
+    std::cout << "*****" << std::endl;
+    std::cout << "   *" << std::endl;
+    std::cout << "  *" << std::endl;
+    std::cout << " *" << std::endl;
+    std::cout << "*****" << std::endl;
+    return 0;
+
+}
